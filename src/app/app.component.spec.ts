c72ca6d44288c2d0fb9cd5dfaf24763ed233ac93
Namito Yokota <namitoyokota@icloud.com>
2023-03-23 21:10:26 -0500

    updated names

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'common'`, () => {
+    it(`should have as title 'ny-components'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('common');
+        expect(app.title).toEqual('ny-components');
     });
 
     it('should render title', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain('common app is running!');
+        expect(compiled.querySelector('.content span')?.textContent).toContain('ny-components app is running!');
     });
 });
