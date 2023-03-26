dcbe07461c080f0f52816a03938e34925ae2240a
Namito Yokota <namitoyokota@icloud.com>
2023-03-26 18:48:43 -0500

    use new ng-components package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NyHyperlinkModule, NyIconModule, NyImageModule } from '@namitoyokota/components';
+import { NyHyperlinkModule, NyIconModule, NyImageModule } from '@namitoyokota/ng-components';
 
 import { AppComponent } from './app.component';
 
