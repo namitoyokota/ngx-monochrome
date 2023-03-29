dfeea69d41c1bc1a5696f1cde984070921389ba1
Namito Yokota <namitoyokota@icloud.com>
2023-03-28 20:54:38 -0500

    created hyperlink

--- projects/ng-components/src/lib/ny-hyperlink/ny-hyperlink.component.ts
+++ projects/ng-components/src/lib/ny-hyperlink/ny-hyperlink.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'ny-hyperlink',
-    template: ` <p>ny-hyperlink</p> `,
-    styles: [],
+    templateUrl: './ny-hyperlink.component.html',
+    styleUrls: ['./ny-hyperlink.component.scss'],
 })
-export class NyHyperlinkComponent {}
+export class NyHyperlinkComponent {
+    /** Link to navigate on click */
+    @Input() src = '';
+
+    /** Text to display to user */
+    @Input() text = '';
+
+    /** Dark mode */
+    @Input() dark = false;
+}
