601cd7bc8aa57cf443f9eb727cfee0e1d4aa3d84
Namito Yokota <namitoyokota@icloud.com>
2023-03-31 21:42:52 -0500

    updated icon testbed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,27 @@ import { NyColor, NyFitType, NyIconConfig, NyIconSize, NyIconType, NySize } from
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    readonly iconConfig1 = new NyIconConfig('fa-home', NyIconType.solid, NyIconSize.small);
-    readonly iconConfig2 = new NyIconConfig('fa-magnifying-glass', NyIconType.solid, NyIconSize.small, NyColor.red);
-    readonly iconConfig3 = new NyIconConfig('fa-magnifying-glass', NyIconType.solid, NyIconSize.medium, NyColor.blue);
-    readonly iconConfig4 = new NyIconConfig('fa-magnifying-glass', NyIconType.solid, NyIconSize.large, NyColor.yellow);
-    readonly iconConfig5 = new NyIconConfig('fa-magnifying-glass', NyIconType.solid, NyIconSize.large, NyColor.white);
+    // Small Icon Configurations
+    readonly iconConfig1 = new NyIconConfig('fa-star', NyIconType.solid, NyIconSize.small, NyColor.black);
+    readonly iconConfig2 = new NyIconConfig('fa-star', NyIconType.solid, NyIconSize.small, NyColor.red);
+    readonly iconConfig3 = new NyIconConfig('fa-star', NyIconType.solid, NyIconSize.small, NyColor.blue);
+    readonly iconConfig4 = new NyIconConfig('fa-star', NyIconType.solid, NyIconSize.small, NyColor.yellow);
+    readonly iconConfig5 = new NyIconConfig('fa-star', NyIconType.solid, NyIconSize.small, NyColor.white);
+
+    // Medium Icon Configurations
+    readonly iconConfig6 = new NyIconConfig('fa-bookmark', NyIconType.solid, NyIconSize.medium, NyColor.black);
+    readonly iconConfig7 = new NyIconConfig('fa-bookmark', NyIconType.solid, NyIconSize.medium, NyColor.red);
+    readonly iconConfig8 = new NyIconConfig('fa-bookmark', NyIconType.solid, NyIconSize.medium, NyColor.blue);
+    readonly iconConfig9 = new NyIconConfig('fa-bookmark', NyIconType.solid, NyIconSize.medium, NyColor.yellow);
+    readonly iconConfig10 = new NyIconConfig('fa-bookmark', NyIconType.solid, NyIconSize.medium, NyColor.white);
+
+    // Large Icon Configurations
+    readonly iconConfig11 = new NyIconConfig('fa-magnifying-glass', NyIconType.solid, NyIconSize.large, NyColor.black);
+    readonly iconConfig12 = new NyIconConfig('fa-magnifying-glass', NyIconType.solid, NyIconSize.large, NyColor.red);
+    readonly iconConfig13 = new NyIconConfig('fa-magnifying-glass', NyIconType.solid, NyIconSize.large, NyColor.blue);
+    readonly iconConfig14 = new NyIconConfig('fa-magnifying-glass', NyIconType.solid, NyIconSize.large, NyColor.yellow);
+    readonly iconConfig15 = new NyIconConfig('fa-magnifying-glass', NyIconType.solid, NyIconSize.large, NyColor.white);
+
     readonly fit = NyFitType.width;
     readonly NyColor: typeof NyColor = NyColor;
     readonly NySize: typeof NySize = NySize;
